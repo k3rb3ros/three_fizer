5de109a0c12a644d7f9a9a68311b3e136e68199a
k3rb3ros <k3rb3ros@k3rb3ros.com>
2014-07-27 23:26:13 -0800

    Added functions to hash password to block cipher block size

--- src/cbc.c
+++ src/cbc.c
@@ -1,11 +1,60 @@
 #include "include/cbc.h"
 
+SkeinSize_t SkeinSize()
+{
+    switch(block_size)
+    {
+        case 256:
+	    return Skein256;
+	break;
+	case 512:
+ 	    return Skein512;
+	break;
+	case 1024:
+	    return Skein1024;
+	break;
+	default:
+	    return Skein512;
+	break;	
+    }
+}
+
 void cbc_decrypt(uint8_t* password, uint64_t* cipher_text, uint64_t size)
 {
-
+    init_state();
+    hash_password(password);
 }
 
 void cbc_encrypt(uint8_t* password, uint64_t* plain_text, uint64_t size)
 {
-	printf("Cbc encrypt\n");
+    init_state();
+    hash_password(password);
 }
+
+void cbc_teardown()
+{
+    if(key_buffer != NULL) free(key_buffer);
+}
+
+void hash_password(uint8_t* password)
+{
+    skeinCtxPrepare(&skein_state, SkeinSize());
+    skeinInit(&skein_state, block_size/8);
+    skeinUpdate(&skein_state, password, strlen((char*)password));
+    skeinFinal(&skein_state, (uint8_t*)key_buffer);
+}
+
+void init_state()
+{
+    key_buffer = calloc(block_size/64, sizeof(uint64_t));
+    switch(block_size)
+    {
+        case 256:
+	break;
+	case 512:
+	break;
+	case 1024:
+	break;
+    }
+}
+
--- src/include/cbc.h
+++ src/include/cbc.h
@@ -3,9 +3,11 @@
 
 #include "skeinApi.h"
 #include "threefishApi.h"
-
+#include "threefizer.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //TODO Finish these functions
 //Cipher block chaining mode for threefish encryption and decryption
@@ -13,16 +15,20 @@
 
 struct SkeinCtx skein_state; //Skein state
 
+uint64_t* key_buffer = NULL;
+
 ThreefishKey_t key; //Threefish key
 
-uint64_t* hash_password(uint8_t* password);
+SkeinSize_t SkeinSize();
 
 void cbc_decrypt(uint8_t* pw, uint64_t* buffer, uint64_t size);
 
 void cbc_encrypt(uint8_t* pw, uint64_t* buffer, uint64_t size);
 
+void cbc_teardown();
+
 void init_state();
 
-void teardown();
+void hash_password(uint8_t* password);
 
 #endif
