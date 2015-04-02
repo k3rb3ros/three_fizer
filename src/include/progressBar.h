10e247979293af34b937bbf9f9ed42570afb3109
Trung Pham <phambaotrung@gmail.com>
2015-04-02 09:13:39 -0700

    Some more align

--- src/include/progressBar.h
+++ src/include/progressBar.h
@@ -1,12 +1,12 @@
 #ifndef PROGRESSBAR_H
 #define PROGRESSBAR_H
 
-#include <pthread.h> //pthead_mutex_t
-#include <stdint.h> //uintxx_t types
-#include <stdio.h> //printf()
-#include <sys/ioctl.h> //ioctl()
-#include <unistd.h> //usleep()
-#include "arguments.h" //arguments structure
+#include <pthread.h>    //pthead_mutex_t
+#include <stdint.h>     //uintxx_t types
+#include <stdio.h>      //printf()
+#include <sys/ioctl.h>  //ioctl()
+#include <unistd.h>     //usleep()
+#include "arguments.h"  //arguments structure
 
 #define STD_TERM_WIDTH 80
 #define SUBTRACT_SIZE 19
@@ -19,7 +19,7 @@ typedef struct
     uint64_t progress;
 } progress_t;
 
-/*Print an asci progress bar of the form "Progress[############/             ]50%"
+/*Print an ascii progress bar of the form "Progress[############/             ]50%"
  * Loops until operations = progress
  */
 void printProgressBar(bool* running, int* error, progress_t* p);
--- src/include/readThread.h
+++ src/include/readThread.h
@@ -1,15 +1,15 @@
 #ifndef READTHREAD_H
 #define READTHREAD_H
 
-#include <pthread.h> //pthread_mutex_t type
-#include <stdbool.h> //bool
-#include <unistd.h> //close()
-#include "chunk.h" //chunk type and createChunk()
-#include "chunkQueue.h" //queue type and enque()
-#include "error.h" //error codes
-#include "pad.h" //pad()
-#include "progressBar.h" //progress_t type
-#include "util.h" //isGreaterThanThreeBlocks()
+#include <pthread.h>        //pthread_mutex_t type
+#include <stdbool.h>        //bool
+#include <unistd.h>         //close()
+#include "chunk.h"          //chunk type and createChunk()
+#include "chunkQueue.h"     //queue type and enque()
+#include "error.h"          //error codes
+#include "pad.h"            //pad()
+#include "progressBar.h"    //progress_t type
+#include "util.h"           //isGreaterThanThreeBlocks()
 
 typedef struct
 {
