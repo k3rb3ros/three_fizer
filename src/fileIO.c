6003f7146a4302b751ee609325ec6b3701455a13
k3rb3ros <k3rb3ros@k3rb3ros.ninja>
2017-04-08 00:59:38 -0700

    Remove call to fsync to speed up chunk write time

--- src/fileIO.c
+++ src/fileIO.c
@@ -16,8 +16,7 @@ bool writeBytes(const uint8_t* data, const uint64_t data_size, int64_t write_fd)
 {
     int64_t status = write(write_fd, data, data_size);
 
-    if(status < (int64_t)data_size) { return false; }
-    if(fsync(write_fd) != 0) { return false; }
+    if (status < (int64_t)data_size) { return false; }
 
     return true;
 }
@@ -37,12 +36,12 @@ inline int openForWrite(const uint8_t* fname)
 
 uint8_t* readBytes(const uint64_t data_size, int64_t fd)
 {
-    if(fd < 0 || data_size == 0) { return NULL; }
+    if (fd < 0 || data_size == 0) { return NULL; }
 
     uint8_t* data = calloc(data_size, sizeof(uint8_t));
-    if(data == NULL) { return NULL; }
+    if (data == NULL) { return NULL; }
 
-    if(read(fd, data, data_size) < (int64_t)data_size)
+    if (read(fd, data, data_size) < (int64_t)data_size)
     {
         free(data);
 	    return NULL;
